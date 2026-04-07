619133738d328976f8d6682ecb04d588dbee6d55
Le1den <tugsuu.80051871@gmail.com>
2026-04-07 22:17:07 +0800

    Update StringProcessor.java

--- src/StringProcessor.java
+++ src/StringProcessor.java
@@ -2,15 +2,24 @@ import java.util.*;
 import java.util.function.*;
 
 public class StringProcessor {
-	public static void processStrings(List<String> strings,
-			Predicate<String> filter,
-			Function<String, String> transformer,
-			Consumer<String> printer) {
-		// Хэрэгжүүл: filter, transform, print хий
-	}
+    public static void processStrings(List<String> strings,
+            Predicate<String> filter,
+            Function<String, String> transformer,
+            Consumer<String> printer) {
+        for (String s : strings) {
+            if (filter.test(s)) {
+                printer.accept(transformer.apply(s));
+            }
+        }
+    }
 
-	public static void main(String[] args) {
-		List<String> words = Arrays.asList("Java", "Програмчлал", "Код");
-		// Хэрэгжүүл: Predicate, Function, Consumer-ийг тодорхойлж, processStrings дууд
-	}
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("Java", "Програмчлал", "Код");
+
+        Predicate<String> lengthFilter = s -> s.length() > 5;
+        Function<String, String> reverser = s -> new StringBuilder(s).reverse().toString();
+        Consumer<String> printer = s -> System.out.println("ГАРШИЛ: " + s.toUpperCase());
+
+        processStrings(words, lengthFilter, reverser, printer);
+    }
 }
